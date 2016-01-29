11bf01ea53400bbdd5e8598a7fcf5a0ff3850ff4
Matt E Patterson <mattp@digimonkey.com>
2016-01-29 11:34:06 -0600

    push

---
+++ nginx/attributes/customize.rb
@@ -0,0 +1,13 @@
+override[:nginx][:gzip_types] = [
+  "application/x-javascript",
+  "application/javascript",
+  "application/xhtml+xml",
+  "application/xml",
+  "application/xml+rss",
+  "application/json",
+  "text/css",
+  "text/javascript",
+  "text/plain",
+  "text/xml",
+  "image/svg+xml"
+]
---
+++ owdocker/recipes/install.rb
@@ -0,0 +1,36 @@
+# include_recipe 'apt'
+# package 'apt-transport-https'
+#
+# apt_repository "docker" do
+#   uri "https://get.docker.com/ubuntu"
+#   distribution "docker"
+#   components ["main"]
+#   keyserver "hkp://keyserver.ubuntu.com:80"
+#   key "36A1D7869245C8950F966E92D8576A8BA88D21E9"
+#   not_if { File.exists?("/etc/apt/sources.list.d/docker.list") }
+# end
+#
+# # Install Docker latest version
+# package "docker" do
+#   package_name "lxc-docker"
+#   action :install
+# end
+#
+# service "docker" do
+#   action :start
+# end
+
+case node[:platform]
+when "ubuntu","debian"
+  package "docker.io" do
+    action :install
+  end
+when 'centos','redhat','fedora','amazon'
+  package "docker" do
+    action :install
+  end
+end
+
+service "docker" do
+  action :start
+end
